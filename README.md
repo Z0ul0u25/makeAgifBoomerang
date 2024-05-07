56305be355ef0f1cc745ecd293919befa1345d6f
Z0ul0u25 <minezoulou25@gmail.com>
2024-05-06 21:13:56 -0400

    premier commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# makeAgifBoomerang
+
+Un script bien simple pour faire boucler un GIF sur lui même en le passant à reculons
+
+## Dépandance
+
+- ffmpeg
+
+## Comment l'utiliser
+
+`./makeAgifBoomerang.sh <TonGif.gif> [frame de début] [frame depuis la fin] [fps]`
+Remplace `TonGif.gif` par le nom de ton image.
+`[frame de début]` Défaut à 0; correspond à l'index du frame de départ.
+`[frame depuis la fin] Défaut à 0; correspond à l'index du frame de fin depuis la fin de l'image. Si le gif à 30 images, si l'option est défini à 5, le dernier frame sera le 25e (30-5).
+`fps` Défaut à 20; est le nombre d'image par seconde du résultat.
+
+Le fichier résultant sera nommé selon le fichier initial plus le frame initial, final et fps exemple: `exemple_2-15-30.gif`
+
+Donc avec la commande `./makeAgifBoomerang..sh exemple.gif 2 15 30` on passe de
+
+![exempleIn](exemple.gif)
+
+Deviendra
+
+![exempleOut](exemple_2-15-30.gif)
---
+++ makeAgifBoomerang.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+	echo "Aucun nom de fichier donner"
+	echo "Format de commande: .sh <Nom_fichier> [premier frame] [nb frame à retirer depuis la fin] [fps]"
+	return 1
+fi
+
+filename="$1"
+if [ -z "$2" ]; then
+	bufferDebut=0
+else
+	bufferDebut=$2
+fi
+
+if [ -z "$3" ]; then
+	bufferFin=0
+else
+	bufferFin=$3
+fi
+# $2 => start buff
+# $3 => end buff
+resultDir="${filename%.*}_boomerang"
+framecount=$(ffprobe -v error -select_streams v:0 -count_packets -show_entries stream=nb_read_packets -of csv=p=0 $1)
+# LOG
+echo "Fichier:	${filename}"
+echo "Frame de début:	${bufferDebut}"
+echo "Frame avant la fin:	${bufferFin}"
+echo "Nombre de frame: ${framecount} > $((framecount-2))"
+
+if [ $framecount -lt $bufferFin ]; then
+	echo "le frame de buffer de fin fourni (${bufferFin}) est trop grand. Le fichier ne contient que ${framecount} frames"
+	return 2
+fi
+
+if [ $framecount -lt $bufferDebut ]; then
+	echo "le frame de buffer de debut fourni (${bufferDebut}) est trop grand. Le fichier ne contient que ${framecount} frames"
+	return 3
+fi
+
+if [ $((framecount-bufferFin)) -le $bufferDebut ]; then
+	echo "Le frame de début est après la fin. le gif à"
+	return 4
+fi
+
+if [ ! -d $resultDir ]; then
+	mkdir "${resultDir}"
+fi
+
+# export frame initial
+ffmpeg -hide_banner -loglevel error -i $filename $resultDir/%04d.png
+
+IMAGES=$resultDir/*
+
+# Compteur d'images
+NB_IMAGES=0
+for f in $IMAGES; do
+	((NB_IMAGES++))
+done
+tmp_nb_image=$NB_IMAGES
+# Enlève les buffer
+for f in $IMAGES; do
+	i=${f: -8: 4}
+	if [ $i -lt $bufferDebut ]; then
+		# echo $i est moins que $bufferDebut
+		rm $f
+		((tmp_nb_image--))
+	elif [ $i -gt $(($NB_IMAGES - $bufferFin)) ]; then
+		# echo $i est plus que $NB_IMAGES - $bufferFin = $(($NB_IMAGES - $bufferFin))
+		rm $f
+		((tmp_nb_image--))
+	fi
+done
+NB_IMAGES=$tmp_nb_image
+
+# Renome pour que les nombres se suivent
+i=0000
+if [ ! $bufferDebut -eq 0 ]; then
+	for f in $IMAGES; do
+		((i++))
+		mv ${f} ${f::-8}$(printf %04d $i).png
+	done
+fi
+
+for i in $(seq 2 $((NB_IMAGES-1))); do
+	n=$((i-2))
+	cp "$resultDir/$(printf %04d $i).png" "$resultDir/$(printf %04d $((NB_IMAGES*2-2-n))).png"
+done
+
+# ==== The meat ====
+# ffmpeg -hide_banner -loglevel error -i $filename -filter_complex "[0:v]trim=start_frame=${bufferDebut}:end_frame=$((framecount-bufferFin)),split=2[a][b];[b]trim=start_frame=1:end_frame=$((framecount-bufferFin-bufferDebut-1)),reverse[c];[a][c]concat" -an "${resultDir}/%04d.png"
+# FUCK THE MEAT
+
+if [ -z "$4" ]; then
+	fps=20
+else
+	fps=$4
+fi
+echo "fps:	${fps}"
+gifski --fps ${fps} -o ${resultDir}_${bufferDebut}-${bufferFin}-${fps}.gif ${resultDir}/*
+
+rm -r $resultDir
