fc7e9b75c4b7ce751d5fdb945f4d05c2f157bda4
Z0ul0u25 <minezoulou25@gmail.com>
2024-05-20 21:57:43 -0400

    deuxième argument maintenant nombre de frame

--- README.md
+++ README.md
@@ -9,18 +9,18 @@ Un script bien simple pour faire boucler un GIF sur lui même en le passant à r
 
 ## Comment l'utiliser
 
-`./makeAgifBoomerang.sh <TonGif.gif> [frame de début] [frame depuis la fin] [fps]`
+`./makeAgifBoomerang.sh <TonGif.gif> [frame de début] [nombre de frame] [fps]`
 Remplace `TonGif.gif` par le nom de ton image.
 
 `[frame de début]` Défaut à 0; correspond à l'index du frame de départ.
 
-`[frame depuis la fin]` Défaut à 0; correspond à l'index du frame de fin depuis la fin de l'image. Si le gif à 30 images, si l'option est défini à 5, le dernier frame sera le 25e (30-5).
+`[nombre de frame]` Défaut à 12; correspond au nombre de frame avant de boucler.
 
 `fps` Défaut à 20; est le nombre d'image par seconde du résultat.
 
 Le fichier résultant sera nommé selon le fichier initial plus le frame initial, final et fps exemple: `exemple_2-15-30.gif`
 
-Donc avec la commande `./makeAgifBoomerang..sh exemple.gif 2 15 30` on passe de
+Donc avec la commande `./makeAgifBoomerang.sh exemple.gif 2 15 30` on passe de
 
 ![exempleIn](exemple.gif)
 
--- makeAgifBoomerang.sh
+++ makeAgifBoomerang.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z "$1" ]; then
 	echo "Aucun nom de fichier donner"
-	echo "Format de commande: .sh <Nom_fichier> [premier frame] [nb frame à retirer depuis la fin] [fps]"
+	echo "Format de commande: .sh <Nom_fichier> [premier frame] [nb frame total] [fps]"
 	return 1
 fi
 
@@ -13,9 +13,9 @@ else
 fi
 
 if [ -z "$3" ]; then
-	bufferFin=0
+	nbFrame=24
 else
-	bufferFin=$3
+	nbFrame=$3
 fi
 # $2 => start buff
 # $3 => end buff
@@ -23,23 +23,19 @@ resultDir="${filename%.*}_boomerang"
 framecount=$(ffprobe -v error -select_streams v:0 -count_packets -show_entries stream=nb_read_packets -of csv=p=0 $1)
 # LOG
 echo "Fichier:	${filename}"
+echo "Nombre de frame: ${framecount}"
+echo "==============="
 echo "Frame de début:	${bufferDebut}"
-echo "Frame avant la fin:	${bufferFin}"
-echo "Nombre de frame: ${framecount} > $((framecount-2))"
-
-if [ $framecount -lt $bufferFin ]; then
-	echo "le frame de buffer de fin fourni (${bufferFin}) est trop grand. Le fichier ne contient que ${framecount} frames"
-	return 2
-fi
+echo "Frame au total:	${nbFrame}"
 
 if [ $framecount -lt $bufferDebut ]; then
-	echo "le frame de buffer de debut fourni (${bufferDebut}) est trop grand. Le fichier ne contient que ${framecount} frames"
+	echo "le frame de debut fourni (${bufferDebut}) est trop grand. Le fichier ne contient que ${framecount} frames"
 	return 3
 fi
 
-if [ $((framecount-bufferFin)) -le $bufferDebut ]; then
-	echo "Le frame de début est après la fin. le gif à"
-	return 4
+if [ $framecount -lt $((nbFrame-bufferDebut)) ]; then
+	echo "le nombre de frame founri (${nbFrame}) est trop grand. Le fichier ne contient que ${framecount} frames"
+	return 2
 fi
 
 if [ ! -d $resultDir ]; then
@@ -64,8 +60,8 @@ for f in $IMAGES; do
 		# echo $i est moins que $bufferDebut
 		rm $f
 		((tmp_nb_image--))
-	elif [ $i -gt $(($NB_IMAGES - $bufferFin)) ]; then
-		# echo $i est plus que $NB_IMAGES - $bufferFin = $(($NB_IMAGES - $bufferFin))
+	elif [ $i -gt $(($bufferDebut + $nbFrame - 1)) ]; then
+		# echo $i est plus que $bufferDebut + $nbFrame = $(($bufferDebut + $nbFrame))
 		rm $f
 		((tmp_nb_image--))
 	fi
@@ -87,7 +83,7 @@ for i in $(seq 2 $((NB_IMAGES-1))); do
 done
 
 # ==== The meat ====
-# ffmpeg -hide_banner -loglevel error -i $filename -filter_complex "[0:v]trim=start_frame=${bufferDebut}:end_frame=$((framecount-bufferFin)),split=2[a][b];[b]trim=start_frame=1:end_frame=$((framecount-bufferFin-bufferDebut-1)),reverse[c];[a][c]concat" -an "${resultDir}/%04d.png"
+# ffmpeg -hide_banner -loglevel error -i $filename -filter_complex "[0:v]trim=start_frame=${bufferDebut}:end_frame=$((framecount-nbFrame)),split=2[a][b];[b]trim=start_frame=1:end_frame=$((framecount-nbFrame-bufferDebut-1)),reverse[c];[a][c]concat" -an "${resultDir}/%04d.png"
 # FUCK THE MEAT
 
 if [ -z "$4" ]; then
@@ -96,6 +92,6 @@ else
 	fps=$4
 fi
 echo "fps:	${fps}"
-gifski --extra -W 2560 -H 1080 --fps ${fps} -o ${resultDir}_${bufferDebut}-${bufferFin}-${fps}.gif ${resultDir}/*
+gifski --extra -W 2560 -H 1080 --fps ${fps} -o ${resultDir}_${bufferDebut}-${nbFrame}-${fps}.gif ${resultDir}/*
 
 rm -r $resultDir
